cd8e08f9c811650adab3109fb61c0398653df828
NamPE286 <nambuihung654@gmail.com>
2022-07-25 21:04:27 +0700

    fix

--- src/routes/players/[id]/index.js
+++ src/routes/players/[id]/index.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
-import AES from 'crypto-js/aes'
 import 'dotenv/config'
 
 export async function GET({params}) {
@@ -7,12 +6,7 @@ export async function GET({params}) {
     const { data, error } = await supabase
         .from('players')
         .select('*')
-        .eq('id', params.id)
-    var a = AES.encrypt(data[0].email, data[0].password).toString()
-    data[0].email = a
-    delete data[0].password
-    //var b = AES.decrypt(a, 'a')
-    //console.log(enc.stringify(b))
+        .eq('uid', params.id)
     return {
         status: 200,
         headers: {
