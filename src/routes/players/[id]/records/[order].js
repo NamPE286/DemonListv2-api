31a3af21ed959fb821baa1287f018a772bea64be
NamPE286 <nambuihung654@gmail.com>
2022-08-08 14:43:45 +0700

    update

--- src/routes/players/[id]/records/[order].js
+++ src/routes/players/[id]/records/[order].js
@@ -4,15 +4,17 @@ import 'dotenv/config'
 export async function GET({params}) {
     const supabase = createClient(import.meta.env.VITE_API_URL, import.meta.env.VITE_API_KEY);
     var { data, error } = await supabase
-        .from('players')
-        .select('*')
-        .eq('uid', params.id)
+        .from('records')
+        .select('*, levels(name)')
+        .eq('userid', params.id)
+        .order(params.order, {ascending: false})
+    console.log(params, error)
     return {
         status: 200,
         headers: {
             'access-control-allow-origin': '*'
         },
-        body: data[0]
+        body: data
     };
 
 }
